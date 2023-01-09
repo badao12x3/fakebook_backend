0846c222f4d76188a4dd25c1593d065b3691a840
dungnd <dzungdducws@gamil.com>
2023-01-09 18:01:43 +0700

    Cài đặt thêm dữ liệu friends và request_friend

--- routes/account.route.js
+++ routes/account.route.js
@@ -4,7 +4,15 @@ const accountRouter = express.Router();
 const accountsController = require("../controllers/account.controller");
 const auth = require("../middlewares/auth.middleware");
 
-accountRouter.post('/login', accountsController.login);
-accountRouter.post('/signup', accountsController.signup);
-
+accountRouter.post("/login", accountsController.login);
+accountRouter.post("/signup", accountsController.signup);
+accountRouter.post("/set_accept_friend", accountsController.set_accept_friend);
+accountRouter.post(
+  "/set_request_friend",
+  accountsController.set_request_friend
+);
+accountRouter.get(
+  "/get_requested_friends",
+  accountsController.get_requested_friends
+);
 module.exports = accountRouter;
