2eb32d73fd3420f9fc320d6dfd1d929db03cc8a6
dungnd <dzungdducws@gamil.com>
2023-02-04 21:48:47 +0700

    thay đổi request method của get_requested_friends

--- routes/account.route.js
+++ routes/account.route.js
@@ -7,17 +7,17 @@ const uploadAvatarOrCoverImageMiddleware = require("../middlewares/uploadAvatarO
 
 accountRouter.post("/login", accountsController.login);
 accountRouter.post("/signup", accountsController.signup);
-accountRouter.post("/del_request_friend",auth, accountsController.del_request_friend);
-accountRouter.post("/set_accept_friend",auth, accountsController.set_accept_friend);
+accountRouter.post("/del_request_friend", auth, accountsController.del_request_friend);
+accountRouter.post("/set_accept_friend", auth, accountsController.set_accept_friend);
 accountRouter.post(
-  "/set_request_friend",auth,
+  "/set_request_friend", auth,
   accountsController.set_request_friend
 );
-accountRouter.post(
-  "/get_requested_friends",auth,
+accountRouter.get(
+  "/get_requested_friends", auth,
   accountsController.get_requested_friends
 );
 accountRouter.get('/get_user_info', auth, accountsController.get_user_info);
-accountRouter.post('/set_user_info',uploadAvatarOrCoverImageMiddleware, auth, accountsController.set_user_info);
+accountRouter.post('/set_user_info', uploadAvatarOrCoverImageMiddleware, auth, accountsController.set_user_info);
 
 module.exports = accountRouter;
