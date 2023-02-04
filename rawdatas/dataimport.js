f586191d025283311defd5806ed1e1bc080ee4aa
dungnd <dzungdducws@gamil.com>
2023-02-04 10:55:37 +0700

    import data post + cmt, sửa rawdata post

--- rawdatas/dataimport.js
+++ rawdatas/dataimport.js
@@ -13,6 +13,192 @@ const { posts, comments } = require("./post.rawdata");
 const accounts = require("./account.rawdata");
 const videos = require("./video.rawdata");
 
+var nouns = [
+  "bird",
+  "clock",
+  "boy",
+  "plastic",
+  "duck",
+  "teacher",
+  "old lady",
+  "professor",
+  "hamster",
+  "dog",
+];
+var verbs = [
+  "kicked",
+  "ran",
+  "flew",
+  "dodged",
+  "sliced",
+  "rolled",
+  "died",
+  "breathed",
+  "slept",
+  "killed",
+];
+var adjectives = [
+  "beautiful",
+  "lazy",
+  "professional",
+  "lovely",
+  "dumb",
+  "rough",
+  "soft",
+  "hot",
+  "vibrating",
+  "slimy",
+];
+var adverbs = [
+  "slowly",
+  "elegantly",
+  "precisely",
+  "quickly",
+  "sadly",
+  "humbly",
+  "proudly",
+  "shockingly",
+  "calmly",
+  "passionately",
+];
+var preposition = [
+  "down",
+  "into",
+  "up",
+  "on",
+  "upon",
+  "below",
+  "above",
+  "through",
+  "across",
+  "towards",
+];
+var status = [
+  "hạnh phúc",
+  "có phúc",
+  "được yêu",
+  "buồn",
+  "đáng yêu",
+  "biết ơn",
+  "hào hứng",
+  "đang yêu",
+  "điên",
+  "cảm kích",
+  "sung sướng",
+  "tuyệt vời",
+  "ngốc nghếch",
+  "vui vẻ",
+  "tuyệt vời",
+  "thật phong cách",
+  "thú vị",
+  "thư giãn",
+  "positive",
+  "rùng mình",
+  "đầy hi vọng",
+  "hân hoan",
+  "mệt mỏi",
+  "có động lực",
+  "proud",
+  "chỉ có một mình",
+  "chu đáo",
+  "OK",
+  "nhớ nhà",
+  "giận dữ",
+  "ốm yếu",
+  "hài lòng",
+  "kiệt sức",
+  "xúc động",
+  "tự tin",
+  "rất tuyệt",
+  "tươi mới",
+  "quyết đoán",
+  "kiệt sức",
+  "bực mình",
+  "vui vẻ",
+  "gặp may",
+  "đau khổ",
+  "buồn tẻ",
+  "buồn ngủ",
+  "tràn đầy sinh lực",
+  "đói",
+  "chuyên nghiệp",
+  "đau đớn",
+  "thanh thản",
+  "thất vọng",
+  "lạc quan",
+  "lạnh",
+  "dễ thương",
+  "tuyệt cú mèo",
+  "thật tuyệt",
+  "hối tiếc",
+  "thật giỏi",
+  "lo lắng",
+  "vui nhộn",
+  "tồi tệ",
+  "xuống tinh thần",
+  "đầy cảm hứng",
+  "hài lòng",
+  "phấn khích",
+  "bình tĩnh",
+  "bối rối",
+  "goofy",
+  "trống vắng",
+  "tốt",
+  "mỉa mai",
+  "cô đơn",
+  "mạnh mẽ",
+  "lo lắng",
+  "đặc biệt",
+  "chán nản",
+  "vui vẻ",
+  "tò mò",
+  "ủ dột",
+  "được chào đón",
+  "gục ngã",
+  "xinh đẹp",
+  "tuyệt vời",
+  "cáu",
+  "căng thẳng",
+  "thiếu",
+  "kích động",
+  "tinh quái",
+  "kinh ngạc",
+  "tức giận",
+  "buồn chán",
+  "bối rồi",
+  "mạnh mẽ",
+  "phẫn nộ",
+  "mới mẻ",
+  "thành công",
+  "ngạc nhiên",
+  "bối rối",
+  "nản lòng",
+  "tẻ nhạt",
+  "xinh xắn",
+  "khá hơn",
+  "tội lỗi",
+  "an toàn",
+  "tự do",
+  "hoang mang",
+  "già nua",
+  "lười biếng",
+  "tồi tệ hơn",
+  "khủng khiếp",
+  "thoải mái",
+  "ngớ ngẩn",
+  "hổ thẹn",
+  "kinh khủng",
+  "đang ngủ",
+  "khỏe",
+  "nhanh nhẹn",
+  "ngại ngùng",
+  "gay go",
+  "kỳ lạ",
+  "như con người",
+  "bị tổn thương",
+  "khủng khiếp",
+];
+
 importDataRouter.post(
   "/removeAll",
   expressAsyncHandler(async (req, res) => {
@@ -271,195 +457,17 @@ importDataRouter.post(
   })
 );
 
+importDataRouter.get(
+  "/get_post",
+  expressAsyncHandler(async (req, res) => {
+    const post = await Post.find({});
+    res.send(post);
+  })
+);
+
 importDataRouter.post(
   "/make_post",
   expressAsyncHandler(async (req, res) => {
-    var nouns = [
-      "bird",
-      "clock",
-      "boy",
-      "plastic",
-      "duck",
-      "teacher",
-      "old lady",
-      "professor",
-      "hamster",
-      "dog",
-    ];
-    var verbs = [
-      "kicked",
-      "ran",
-      "flew",
-      "dodged",
-      "sliced",
-      "rolled",
-      "died",
-      "breathed",
-      "slept",
-      "killed",
-    ];
-    var adjectives = [
-      "beautiful",
-      "lazy",
-      "professional",
-      "lovely",
-      "dumb",
-      "rough",
-      "soft",
-      "hot",
-      "vibrating",
-      "slimy",
-    ];
-    var adverbs = [
-      "slowly",
-      "elegantly",
-      "precisely",
-      "quickly",
-      "sadly",
-      "humbly",
-      "proudly",
-      "shockingly",
-      "calmly",
-      "passionately",
-    ];
-    var preposition = [
-      "down",
-      "into",
-      "up",
-      "on",
-      "upon",
-      "below",
-      "above",
-      "through",
-      "across",
-      "towards",
-    ];
-    var status = [
-      "hạnh phúc",
-      "có phúc",
-      "được yêu",
-      "buồn",
-      "đáng yêu",
-      "biết ơn",
-      "hào hứng",
-      "đang yêu",
-      "điên",
-      "cảm kích",
-      "sung sướng",
-      "tuyệt vời",
-      "ngốc nghếch",
-      "vui vẻ",
-      "tuyệt vời",
-      "thật phong cách",
-      "thú vị",
-      "thư giãn",
-      "positive",
-      "rùng mình",
-      "đầy hi vọng",
-      "hân hoan",
-      "mệt mỏi",
-      "có động lực",
-      "proud",
-      "chỉ có một mình",
-      "chu đáo",
-      "OK",
-      "nhớ nhà",
-      "giận dữ",
-      "ốm yếu",
-      "hài lòng",
-      "kiệt sức",
-      "xúc động",
-      "tự tin",
-      "rất tuyệt",
-      "tươi mới",
-      "quyết đoán",
-      "kiệt sức",
-      "bực mình",
-      "vui vẻ",
-      "gặp may",
-      "đau khổ",
-      "buồn tẻ",
-      "buồn ngủ",
-      "tràn đầy sinh lực",
-      "đói",
-      "chuyên nghiệp",
-      "đau đớn",
-      "thanh thản",
-      "thất vọng",
-      "lạc quan",
-      "lạnh",
-      "dễ thương",
-      "tuyệt cú mèo",
-      "thật tuyệt",
-      "hối tiếc",
-      "thật giỏi",
-      "lo lắng",
-      "vui nhộn",
-      "tồi tệ",
-      "xuống tinh thần",
-      "đầy cảm hứng",
-      "hài lòng",
-      "phấn khích",
-      "bình tĩnh",
-      "bối rối",
-      "goofy",
-      "trống vắng",
-      "tốt",
-      "mỉa mai",
-      "cô đơn",
-      "mạnh mẽ",
-      "lo lắng",
-      "đặc biệt",
-      "chán nản",
-      "vui vẻ",
-      "tò mò",
-      "ủ dột",
-      "được chào đón",
-      "gục ngã",
-      "xinh đẹp",
-      "tuyệt vời",
-      "cáu",
-      "căng thẳng",
-      "thiếu",
-      "kích động",
-      "tinh quái",
-      "kinh ngạc",
-      "tức giận",
-      "buồn chán",
-      "bối rồi",
-      "mạnh mẽ",
-      "phẫn nộ",
-      "mới mẻ",
-      "thành công",
-      "ngạc nhiên",
-      "bối rối",
-      "nản lòng",
-      "tẻ nhạt",
-      "xinh xắn",
-      "khá hơn",
-      "tội lỗi",
-      "an toàn",
-      "tự do",
-      "hoang mang",
-      "già nua",
-      "lười biếng",
-      "tồi tệ hơn",
-      "khủng khiếp",
-      "thoải mái",
-      "ngớ ngẩn",
-      "hổ thẹn",
-      "kinh khủng",
-      "đang ngủ",
-      "khỏe",
-      "nhanh nhẹn",
-      "ngại ngùng",
-      "gay go",
-      "kỳ lạ",
-      "như con người",
-      "bị tổn thương",
-      "khủng khiếp",
-    ];
-
     const all = await Account.find({});
     let accounts = [];
     all.filter((item) => {
@@ -566,10 +574,94 @@ importDataRouter.post(
   })
 );
 
+importDataRouter.get(
+  "/get_cmt",
+  expressAsyncHandler(async (req, res) => {
+    const cmt = await Comment.find({});
+    res.send(cmt);
+  })
+);
+
 importDataRouter.post(
   "/make_cmt",
   expressAsyncHandler(async (req, res) => {
-    
+    const posts = await Post.find({});
+    var cmts = [];
+    for (let i of posts) {
+      var comments = 0;
+      var commentList = [];
+      if (i["canComment"]) {
+        for (let j = 0; j < Math.floor(Math.random() * 3); j++) {
+          const blockedAccounts = await Account.find({
+            _id: i["account_id"],
+          }).select("blockedAccounts");
+          const accounts = await Account.find({});
+          const shuffled_acc = accounts.sort(() => 0.5 - Math.random());
+          var selected = shuffled_acc.slice(0, 1);
+          var check = true;
+          if (blockedAccounts["blockedAccounts"])
+            blockedAccounts["blockedAccounts"].filter((item) => {
+              if (selected[0]["_id"] == item["account"]) check = false;
+            });
+          if (check) {
+            var rand1 = Math.floor(Math.random() * 10);
+            var rand2 = Math.floor(Math.random() * 10);
+            var rand3 = Math.floor(Math.random() * 10);
+            var rand4 = Math.floor(Math.random() * 10);
+            var rand5 = Math.floor(Math.random() * 10);
+            var rand6 = Math.floor(Math.random() * 10);
+            var content =
+              "The " +
+              adjectives[rand1] +
+              " " +
+              nouns[rand2] +
+              " " +
+              adverbs[rand3] +
+              " " +
+              verbs[rand4] +
+              " because some " +
+              nouns[rand1] +
+              " " +
+              adverbs[rand1] +
+              " " +
+              verbs[rand1] +
+              " " +
+              preposition[rand1] +
+              " a " +
+              adjectives[rand2] +
+              " " +
+              nouns[rand5] +
+              " which, became a " +
+              adjectives[rand3] +
+              ", " +
+              adjectives[rand4] +
+              " " +
+              nouns[rand6] +
+              ".";
+            cmt = {
+              post_id: i["_id"],
+              userComment_id: selected[0]["_id"],
+              content: content,
+            };
+            commentList.push(selected[0]["_id"]);
+            cmts.push(cmt);
+            comments++;
+          }
+        }
+        const filer = {
+          _id: i["_id"],
+        };
+        const update = {
+          $set: {
+            commentList: commentList,
+            comments: comments,
+          },
+        };
+        await Post.updateOne(filer, update);
+      }
+    }
+    const importComments = await Comment.insertMany(cmts);
+    res.send({ importComments });
   })
 );
 
