2027bcbe41adfe0cbd8451d181fcd8cd82a1ffbf
badao12x3 <hoanlazy123@gmail.com>
2023-02-12 10:54:20 +0700

    change ulr video rawdata, add like_video and unlike_video

--- controllers/video.controller.js
+++ controllers/video.controller.js
@@ -54,18 +54,126 @@ videosController.get_list_videos = expressAsyncHandler(async (req, res) => {
                     publicId: video.video.publicId
                 },
                 isAdsCampaign: video.isAdsCampaign,
-                createdAt: video.createdAt.toString(),
-                updatedAt: video.updatedAt.toString(),
+                createdAt: video.createdAt,
+                updatedAt: video.updatedAt,
                 likes: video.likes,
                 is_liked: video.likedAccounts.includes(req.account._id),
             }
         })
     }
+
+    // const result = {
+    //     videos: videos.map(video => {
+    //         return {
+    //             described: video.described,
+    //             video: {
+    //                 filename: video.video.filename,
+    //                 url: video.video.url,
+    //                 publicId: video.video.publicId
+    //             },
+    //             isAdsCampaign: video.isAdsCampaign,
+    //             likes: video.likes,
+    //             likedAccounts: video.likedAccounts,
+    //         }
+    //     })
+    // }
     res.status(responseError.OK.statusCode).json({
         code: responseError.OK.body.code,
         message: responseError.OK.body.message,
         data: result
+        // result
     });
 });
+
+videosController.like_video = expressAsyncHandler(async (req, res) => {
+    const id = req.body.id;
+    const account = req.account;
+    console.log(account)
+    if (id === undefined)
+        return setAndSendResponse(res, responseError.PARAMETER_IS_NOT_ENOUGH);
+    if (!isValidId(id)) {
+        return setAndSendResponse(res, responseError.PARAMETER_VALUE_IS_INVALID);
+    }
+
+    if (account.isBlocked) return setAndSendResponse(res, responseError.NOT_ACCESS);
+
+    try {
+        let video = await Video.findById(id);
+        // console.log("đã tìm được video")
+        // console.log(video)
+        if (video == null) {
+            return setAndSendResponse(res, responseError.VIDEO_IS_NOT_EXISTED);
+        }
+
+        if (
+            video.likedAccounts.findIndex((element) => {
+                return element.equals(account._id);
+            }) != -1
+        )
+            return setAndSendResponse(res, responseError.HAS_BEEN_LIKED);
+        else {
+            console.log("đã qua")
+            await Video.findOneAndUpdate(
+                {_id: id},
+                {$push: {likedAccounts: {_id: account._id}}}
+            );
+            console.log("đã qua")
+            var updatedVideo = await Video.findOneAndUpdate({_id: id}, {$inc: {likes: 1}}, { new: true });
+            res.status(responseError.OK.statusCode).json({
+                code: responseError.OK.body.code,
+                message: responseError.OK.body.message,
+                data: {
+                    likes: updatedVideo.likes
+                }
+            });
+        }
+    } catch (err) {
+        return setAndSendResponse(res, responseError.UNKNOWN_ERROR);
+    }
+});
+
+videosController.unlike_video = expressAsyncHandler(async (req, res) => {
+    const id = req.body.id;
+    const account = req.account;
+    if (id === undefined)
+        return setAndSendResponse(res, responseError.PARAMETER_IS_NOT_ENOUGH);
+    if (!isValidId(id)) {
+        return setAndSendResponse(res, responseError.PARAMETER_VALUE_IS_INVALID);
+    }
+
+    if (account.isBlocked) return setAndSendResponse(res, responseError.NOT_ACCESS);
+
+    try {
+        let video = await Video.findById(id);
+        if (video == null) {
+            return setAndSendResponse(res, responseError.VIDEO_IS_NOT_EXISTED);
+        }
+
+        if (
+            video.likedAccounts.findIndex((element) => {
+                return element.equals(account._id);
+            }) == -1
+        )
+            return setAndSendResponse(res, responseError.HAS_NOT_BEEN_LIKED);
+        else {
+            await Video.findOneAndUpdate(
+                {_id: id},
+                {$pull: {likedAccounts:  account._id}}
+            );
+            var updatedVideo = await Video.findOneAndUpdate({_id: id}, {$inc: {likes: -1}}, { new: true });
+            res.status(responseError.OK.statusCode).json({
+                code: responseError.OK.body.code,
+                message: responseError.OK.body.message,
+                data: {
+                    likes: updatedVideo.likes
+                }
+            })
+            console.log(video.likedAccounts);
+        }
+    } catch (err) {
+        return setAndSendResponse(res, responseError.UNKNOWN_ERROR);
+    }
+});
+
 
 module.exports = videosController;
