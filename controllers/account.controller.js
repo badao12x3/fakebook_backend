47c40e316dce2fddba317e89f0a12f960be2b366
dungnd <dzungdducws@gamil.com>
2023-02-14 07:38:17 +0700

    thêm get lít friend và sửa get info user

--- controllers/account.controller.js
+++ controllers/account.controller.js
@@ -381,6 +381,39 @@ accountsController.get_requested_friends = expressAsyncHandler(async (req, res) 
     }
 });
 
+
+accountsController.get_list_friends = expressAsyncHandler(async (req, res) => {
+    const {user_id} = req.query;
+    const _id = user_id?user_id:req.account._id;
+    if (!_id) {
+        return setAndSendResponse(res, responseError.PARAMETER_IS_NOT_ENOUGH);
+    } else if (!isValidId(_id)) {
+        return setAndSendResponse(res, responseError.PARAMETER_VALUE_IS_INVALID);
+    }
+
+    let account = await Account.findOne({_id: _id}).select("friends -_id" );
+
+    let ListFriends = []
+
+    for (let friend of account["friends"]){
+      let account_id = friend["friend"]
+      let _account = await Account.findOne({_id: account_id}).select(["name", "avatar"]);
+      ListFriends.push({
+        friend: account_id,
+        name: _account["name"],
+        avatar: _account["avatar"].url,
+        createdAt: friend["createdAt"]
+      })
+    }
+
+    if (ListFriends == null) {
+        return setAndSendResponse(res, responseError.NO_DATA);
+    } else {
+        return res.status(responseError.OK.statusCode).json({ListFriends});
+    }
+});
+
+
 accountsController.set_user_info = expressAsyncHandler(async (req, res) => {
     const {username, description, city, country, link} = req.body;
     const {account} = req;
@@ -471,27 +504,12 @@ accountsController.set_user_info = expressAsyncHandler(async (req, res) => {
 
 accountsController.get_user_info = expressAsyncHandler(async (req, res) => {
     const {user_id} = req.query;
-    const {account} = req;
+    const account_id = req.account.id;
 
-    console.log(account);
+    let account = await Account.findOne({_id: account_id});
+
     if (account.isBlocked) return setAndSendResponse(res, responseError.NOT_ACCESS);
 
-    var user = null;
-    let data = {
-        id: null,
-        username: null,
-        created: null,
-        description: null,
-        avatar: null,
-        cover_image: null,
-        link: null,
-        address: null,
-        city: null,
-        country: null,
-        listing: null,
-        is_friend: null,
-        online: null
-    }
 
     if (user_id) {
         if (!isValidId(user_id)) return setAndSendResponse(res, responseError.PARAMETER_VALUE_IS_INVALID);
@@ -500,61 +518,48 @@ accountsController.get_user_info = expressAsyncHandler(async (req, res) => {
 
         if (!user) return setAndSendResponse(res, responseError.NO_DATA);
 
-        console.log(user);
-
         if (user.isBlocked == true) {
             console.log("tài khoản bị block");
             return setAndSendResponse(res, responseError.USER_IS_NOT_VALIDATED);
         }
 
-        if (user.blockedAccounts) {
-            let index = user.blockedAccounts.findIndex(element => element.account.equals(account._id));
-            if (index >= 0) return callRes(res, responseError.USER_IS_NOT_VALIDATED, 'Bạn bị người ta blocked rồi nên không thể lấy info của họ');
-            let index1 = account.blockedAccounts.findIndex(element => element.account.equals(user._id));
-            if (index1 >= 0) return callRes(res, responseError.USER_IS_NOT_VALIDATED, 'Bạn đang blocked user muốn lấy info');
+        for (let i of user["blockedAccounts"]){
+            if (i["account"]==account_id)
+            return callRes(res, responseError.USER_IS_NOT_VALIDATED, 'Bạn bị người ta blocked rồi nên không thể lấy info của họ');
+        }
+        
+        for (let i of account["blockedAccounts"]){
+            if (i["account"]==user_id)
+            return callRes(res, responseError.USER_IS_NOT_VALIDATED, 'Bạn đang blocked user muốn lấy info');
         }
 
-        // const [friend, isBlocked] = await Promise.all([
-        //     FriendList.findOne({$or: [
-        //         {user1_id: account._id, user2_id: user_id},
-        //         {user1_id: user_id, user2_id: account._id}
-        //     ]}),
-        //     FriendBlock.findOne({$or: [
-        //         {accountDoBlock_id: user_id, blockedUser_id: account._id},
-        // 		{accountDoBlock_id: account._id, blockedUser_id: user_id}
-        //     ]})
-        // ]);
+    } 
+    let id = user_id?user_id:account_id
+    let user_info = await Account.findOne({ _id: id }).select([
+      "avatar",
+      "coverImage",
+      "name",
+      "gender",
+      "phoneNumber",
+      "description",
+      "city",
+      "country",
+      "createdAt",
+    ]);
 
-        // if(friend) isFriend = true;
-        // if(isBlocked) return setAndSendResponse(res, responseError.USER_IS_NOT_VALIDATED);
-    } else {
-        user = account;
-    }
-
-    // const friendNum = await FriendList.find({
-    // 	$or: [
-    // 		{user1_id: user._id},
-    // 		{user2_id: user._id}
-    // 	]
-    // }).countDocuments();
-
-    data.id = user._id.toString();
-    data.username = user.name;
-    data.created = Math.floor(user.createdAt.getTime() / 1000);
-    data.description = user.description;
-    data.avatar = user.avatar.url;
-    data.cover_image = user.coverImage.url;
-    data.link = user.link;
-    data.city = user.city;
-    data.country = user.country;
-    data.listing = user.friends.length;
-    data.online = user.online;
-    data.is_friend = false;
-    if (user_id) {
-        let indexExist = user.friends.findIndex(element => element.account.equals(account._id));
-        data.is_friend = (indexExist >= 0) ? true : false;
-    }
-    return callRes(res, responseError.OK, data);
+    let res_user_info = {
+      avatar: user_info["avatar"].url,
+      coverImage: user_info["coverImage"].url,
+      name: user_info["name"],
+      gender: user_info["gender"],
+      phoneNumber: user_info["phoneNumber"],
+      description: user_info["description"],
+      city: user_info["city"],
+      country: user_info["country"],
+      createdAt: user_info["createdAt"],
+    };
+    
+    return res.status(responseError.OK.statusCode).json(res_user_info);
 })
 
 accountsController.change_info_after_signup = expressAsyncHandler(async (req, res) => {
